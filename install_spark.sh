d1143d6d437f6096b1e10cb57d8d4e0475afab23
Emeric Jimenez <64564881+emericjimenez@users.noreply.github.com>
2024-03-25 02:00:02 -0400

    Update install_spark.sh

--- install_spark.sh
+++ install_spark.sh
@@ -38,9 +38,6 @@ mv -f $DATASET /opt/spark/datasets/$DATASET
 cd /opt/spark/bin
 
 echo "from pyspark.sql import SparkSession" >$FILE_PY
-echo "import logging" >$FILE_PY
-echo "logger = spark._jvm.org.apache.log4j" >$FILE_PY
-echo "logger.LogManager.getLogger('org').setLevel(logger.Level.ERROR)" >$FILE_PY
 echo "spark = SparkSession.builder \\" >>$FILE_PY
 echo "    .appName('Lectura de Dataset') \\" >>$FILE_PY
 echo "    .getOrCreate()" >>$FILE_PY
